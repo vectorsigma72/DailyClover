1ee722b04c12ed8ed2e08c3cda2964cd24dde0cf
Travis CI <travis@travis-ci.com>
2019-09-13 18:27:22 +0200

    Corrections

--- .travis/updateRepo.sh
+++ .travis/updateRepo.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 TCICLOVERDIR="$HOME"/src/CloverBootloader
-INITDIR="$TRAVIS_BUILD_DIR"/DailyClover
+
 setuser() {
-  cd "${TCICLOVERDIR}"
+  echo "set user info start.."
+  cd "${TRAVIS_BUILD_DIR}"
   git config --global user.email "$GIT_EMAIL"
   git config --global user.name "$GIT_NAME"
+  echo "set user info end"
 }
 
 exportVariables() {
+  echo "export variables start.."
   cd "${TCICLOVERDIR}"
   export CLOVER_REV="r$(cat vers.txt)"
   export CLOVER_HASH="$(git rev-parse --short HEAD)"
+  echo "export variables end"
 }
 
 
 checkTags() {
-  cd "${INITDIR}"
+  echo "check tags start.."
+  cd "${TRAVIS_BUILD_DIR}"
   local tags=($(git tag))
 
   for tag in "${tags[@]}"
@@ -26,17 +31,20 @@ checkTags() {
       git tag -d $tag
     fi
   done
+  echo "check tags end"
 }
 
 tag() {
-  cd "${INITDIR}"
+  echo "tag changes start.."
+  cd "${TRAVIS_BUILD_DIR}"
   echo "$CLOVER_REV $CLOVER_HASH" > .lastTag
   echo "$(date)" >> .lastTag
   export TRAVIS_TAG="${CLOVER_REV}-${CLOVER_HASH}"
   git add .
   git commit -am "Travis build: ${TRAVIS_TAG}"
-  git push origin master
   git tag "${TRAVIS_TAG}"
+  git push https://${GIT_NAME}:${GH_TOKEN}@github.com/${GIT_NAME}/DailyClover.git --tags
+  echo "tag changes end"
 }
 
 exportVariables
