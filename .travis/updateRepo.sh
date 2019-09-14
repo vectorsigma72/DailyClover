9206e6483c52914ff9374c458a317f57145148c2
vectorsigma72 <mbp140cd@gmail.com>
2019-09-14 22:13:40 +0200

    Update updateRepo.sh

--- .travis/updateRepo.sh
+++ .travis/updateRepo.sh
@@ -5,13 +5,15 @@ checkTags() {
   cd "${TRAVIS_BUILD_DIR}"
   local tags=($(git tag))
 
+  git config --global user.email "$GIT_EMAIL"
+  git config --global user.name "$GIT_NAME"
+
   for t in "${tags[@]}"
   do
     if [[ "$t" == "${CLOVER_REV}"-* ]]; then
       echo "deleting old tag \"$t\""
       git tag -d $t
       git push --delete https://${GIT_NAME}:${GH_TOKEN}@github.com/${GIT_NAME}/DailyClover.git $t
-
     fi
   done
   echo "check tags end"
